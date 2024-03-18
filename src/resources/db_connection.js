70b415cd1945f1eb3ce97e747bd25a73306764da
Lucas Gabriel <lucas.81.gabriel@gmail.com>
2024-03-18 09:17:01 -0300

    ✨ feat: atualizacoes db

--- src/resources/db_connection.js
+++ src/resources/db_connection.js
@@ -2,14 +2,12 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  //   process.env.PG_DATABASE,
-  "",
+  process.env.PG_DATABASE,
   process.env.PG_USER,
-  //   process.env.PG_PASSWORD,
-  "",
+  process.env.PG_PASSWORD,
   {
-    host: "127.0.0.1",
-    dialect: "postgres",
+    host: process.env.PG_HOST,
+    dialect: process.env.PG_DIALECT,
   }
 );
 
