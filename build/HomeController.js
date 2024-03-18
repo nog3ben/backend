87952428918ee13a74b40be99ce6e14cf5309368
Lucas Gabriel <lucas.81.gabriel@gmail.com>
2024-03-18 08:34:44 -0300

    ✨ feat: build removido

--- build/HomeController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const Controller = require("./Controller");
-const ControllerInterface = require("./Interface");
-class HomeController extends Controller {
-    index(req, res) {
-        res.send("Skrrr");
-    }
-}
-module.exports = (req, res) => new HomeController(req, res);
